dfec7660c1440207a841bdd98601201f290be83f
Christopher <araujochristopher715@gmail.com>
2025-01-15 23:13:37 +0100

    el bot posee imagenes y frases completas

--- Jayuwoki/src/main/java/dad/api/EventoPrueba.java
+++ Jayuwoki/src/main/java/dad/api/EventoPrueba.java
@@ -1,6 +1,9 @@
 package dad.api;
 import net.dv8tion.jda.api.events.message.*;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.utils.FileUpload;
+
+import java.io.InputStream;
 
 public class EventoPrueba extends ListenerAdapter {
 
@@ -10,6 +13,20 @@ public class EventoPrueba extends ListenerAdapter {
 
         if (message.equals("marco")) {
             event.getChannel().sendMessage("Polo").queue();
+        } else if (message.equals("hola")) {
+            event.getChannel().sendMessage("En tu culo mi aparato").queue();
+        } else if (message.contains("razón")) {
+            event.getChannel().sendMessage("Que si , Que si").queue();
+        } else if (message.equals("mike")) {
+            event.getChannel().sendMessage("sáquelo a pasear").queue();
+        } else if (message.contains("5")) {
+            InputStream resource = getClass().getResourceAsStream("/images/vegeta.jpg");
+            if (resource == null) {
+                System.out.println("Resource not found: /images/vegeta.jpg");
+            } else {
+                event.getChannel().sendFiles(FileUpload.fromData(resource, "vegeta.jpg")).queue();
+            }
+
         }
     }
 }
