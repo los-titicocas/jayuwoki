fa260d54bb7ad05f9ad02e3cc6ba68bbc7689c67
AfroOrange <nuhacet1998@gmail.com>
2025-01-16 12:56:49 +0000

    refactor: change window order so the splashScreen is the first one

--- Jayuwoki/src/main/java/dad/app/JayuwokiApp.java
+++ Jayuwoki/src/main/java/dad/app/JayuwokiApp.java
@@ -1,32 +1,46 @@
 package dad.app;
 
 import dad.controllers.LoginController;
+import dad.custom.ui.SplashScreenController;
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.util.Objects;
 
 public class JayuwokiApp extends Application {
 
-    LoginController loginController = new LoginController();
+    SplashScreenController splashScreenController = new SplashScreenController();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-//        Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
         String darkTheme = getClass().getResource("/styles/dark-theme.css").toExternalForm();
         Application.setUserAgentStylesheet(darkTheme);
         Image appIcon = new Image(Objects.requireNonNull(getClass().getResource("/images/logo.png")).toString());
 
-        Scene scene = new Scene(loginController.getRoot());
+        Scene scene = new Scene(splashScreenController.getRoot());
         Stage stage = new Stage();
 
         stage.getIcons().add(appIcon);
-        stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
+        fadeIn(splashScreenController.getRoot());
         stage.show();
     }
+
+    public void fadeIn(Node node) {
+        FadeTransition fade = new FadeTransition(Duration.seconds(2), node);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.play();
+    }
+
 }
