6a9ae1b6100ffd2e73b3e1f15a71cd262e337cd7
AfroOrange <nuhacet1998@gmail.com>
2025-01-27 21:01:45 +0000

    feat: add persistance to settings in .properties and finish css for panel

--- Jayuwoki/src/main/java/dad/panels/SettingsController.java
+++ Jayuwoki/src/main/java/dad/panels/SettingsController.java
@@ -4,16 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+import java.io.File;
 
 public class SettingsController implements Initializable {
 
@@ -53,16 +56,79 @@ public class SettingsController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        loadSettings();
     }
 
     @FXML
     void onApplyAction(ActionEvent event) {
+        saveSettings();
 
+        // if any changes are made, it will show a dialog
+
+        Dialog savedDialog = new Dialog();
+        savedDialog.setContentText("Settings saved successfully!");
+        savedDialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        savedDialog.showAndWait();
     }
 
     @FXML
     void onResetAction(ActionEvent event) {
 
+        // show a dialog to confirm the reset
+        Dialog resetDialog = new Dialog();
+        resetDialog.setContentText("Are you sure you want to reset all settings?");
+        resetDialog.getDialogPane().getButtonTypes().add(ButtonType.YES);
+        resetDialog.getDialogPane().getButtonTypes().add(ButtonType.NO);
+        if (resetDialog.showAndWait().get() == ButtonType.YES) {
+            clearCheckBox();
+            saveSettings();
+        }
+    }
+
+    private void loadSettings() {
+        File settingsFile = new File("Jayuwoki/settings.properties");
+        Properties properties = new Properties();
+
+        if (settingsFile.exists()) {
+            try (FileInputStream input = new FileInputStream(settingsFile)) {
+                properties.load(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        imageCheck.setSelected(Boolean.parseBoolean(properties.getProperty("imageCheck", "false")));
+        massPermissionCheck.setSelected(Boolean.parseBoolean(properties.getProperty("massPermissionCheck", "false")));
+        soundCheck.setSelected(Boolean.parseBoolean(properties.getProperty("soundCheck", "false")));
+    }
+
+    private void saveSettings() {
+        File settingsFile = new File("Jayuwoki/settings.properties");
+        Properties properties = new Properties();
+
+        if (settingsFile.exists()) {
+            try (FileInputStream input = new FileInputStream(settingsFile)) {
+                properties.load(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        properties.setProperty("imageCheck", String.valueOf(imageCheck.isSelected()));
+        properties.setProperty("massPermissionCheck", String.valueOf(massPermissionCheck.isSelected()));
+        properties.setProperty("soundCheck", String.valueOf(soundCheck.isSelected()));
+
+        try (FileOutputStream output = new FileOutputStream(settingsFile)) {
+            properties.store(output, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clearCheckBox() {
+        imageCheck.setSelected(false);
+        massPermissionCheck.setSelected(false);
+        soundCheck.setSelected(false);
     }
 
     public BorderPane getRoot() {
